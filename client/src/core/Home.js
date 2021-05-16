204d2711cf15a7db013ae0f5b35e4e181bcef395
DevrajBhatt <60641403+DevrajBhatt@users.noreply.github.com>
2021-05-16 14:54:25 +0530

    changed name

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Posts from "../post/Posts";
 
-const Home = () => (
-  <div>
-    <div className="jumbotron">
-      <h2>Home</h2>
-      <p className="lead">Welcome to React Frontend</p>
-    </div>
-    <div className="container">
-      <Posts />
-    </div>
-  </div>
+const Home = () => ( <
+    div >
+    <
+    div className = "jumbotron" >
+    <
+    h2 >
+    <
+    b > Flash - Takes < /b> <
+    /h2> <
+    p className = "lead" > Welcome Home < /p> <
+    /div> <
+    div className = "container" >
+    <
+    Posts / >
+    <
+    /div> <
+    /div>
 );
 
 export default Home;
