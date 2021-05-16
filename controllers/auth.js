3449643c4ce7bfb7dc909e56e38947ad9f96fd7b
DevrajBhatt <60641403+DevrajBhatt@users.noreply.github.com>
2021-05-16 14:48:27 +0530

    some more jugaad

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,23 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const expressJwt = require('express-jwt');
-const User = require('../models/user');
-const _ = require('lodash');
-const { OAuth2Client } = require('google-auth-library');
-const { sendEmail } = require('../helpers');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const expressJwt = require("express-jwt");
+const User = require("../models/user");
+const _ = require("lodash");
+const { OAuth2Client } = require("google-auth-library");
+const { sendEmail } = require("../helpers");
 
-exports.signup = async (req, res) => {
+const REACT_APP_GOOGLE_CLIENT_ID =
+    "990837452625-1dsqp6j85kfqh6buh6ld3eu24hbmlke9.apps.googleusercontent.com";
+
+exports.signup = async(req, res) => {
     const userExists = await User.findOne({ email: req.body.email });
     if (userExists)
         return res.status(403).json({
-            error: 'Email is taken!'
+            error: "Email is taken!",
         });
     const user = await new User(req.body);
     await user.save();
-    res.status(200).json({ message: 'Signup success! Please login.' });
+    res.status(200).json({ message: "Signup success! Please login." });
 };
 
 exports.signin = (req, res) => {
@@ -24,20 +27,22 @@ exports.signin = (req, res) => {
         // if err or no user
         if (err || !user) {
             return res.status(401).json({
-                error: 'User with that email does not exist. Please signup.'
+                error: "User with that email does not exist. Please signup.",
             });
         }
         // if user is found make sure the email and password match
         // create authenticate method in model and use here
         if (!user.authenticate(password)) {
             return res.status(401).json({
-                error: 'Email and password do not match'
+                error: "Email and password do not match",
             });
         }
         // generate a token with user id and secret
-        const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET);
+        const token = jwt.sign({ _id: user._id, role: user.role },
+            process.env.JWT_SECRET
+        );
         // persist the token as 't' in cookie with expiry date
-        res.cookie('t', token, { expire: new Date() + 9999 });
+        res.cookie("t", token, { expire: new Date() + 9999 });
         // retrun response with user and token to frontend client
         const { _id, name, email, role } = user;
         return res.json({ token, user: { _id, email, name, role } });
@@ -45,44 +50,43 @@ exports.signin = (req, res) => {
 };
 
 exports.signout = (req, res) => {
-    res.clearCookie('t');
-    return res.json({ message: 'Signout success!' });
+    res.clearCookie("t");
+    return res.json({ message: "Signout success!" });
 };
 
 exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
-    userProperty: 'auth'
+    userProperty: "auth",
 });
 
 exports.forgotPassword = (req, res) => {
-    if (!req.body) return res.status(400).json({ message: 'No request body' });
-    if (!req.body.email) return res.status(400).json({ message: 'No Email in request body' });
+    if (!req.body) return res.status(400).json({ message: "No request body" });
+    if (!req.body.email)
+        return res.status(400).json({ message: "No Email in request body" });
 
-    console.log('forgot password finding user with that email');
+    console.log("forgot password finding user with that email");
     const { email } = req.body;
-    console.log('signin req.body', email);
+    console.log("signin req.body", email);
     // find the user based on email
     User.findOne({ email }, (err, user) => {
         // if err or no user
         if (err || !user)
-            return res.status('401').json({
-                error: 'User with that email does not exist!'
+            return res.status("401").json({
+                error: "User with that email does not exist!",
             });
 
         // generate a token with user id and secret
-        const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME }, process.env.JWT_SECRET);
+        const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME },
+            process.env.JWT_SECRET
+        );
 
         // email data
         const emailData = {
-            from: 'noreply@node-react.com',
+            from: "noreply@node-react.com",
             to: email,
-            subject: 'Password Reset Instructions',
-            text: `Please use the following link to reset your password: ${
-                process.env.CLIENT_URL
-            }/reset-password/${token}`,
-            html: `<p>Please use the following link to reset your password:</p> <p>${
-                process.env.CLIENT_URL
-            }/reset-password/${token}</p>`
+            subject: "Password Reset Instructions",
+            text: `Please use the following link to reset your password: ${process.env.CLIENT_URL}/reset-password/${token}`,
+            html: `<p>Please use the following link to reset your password:</p> <p>${process.env.CLIENT_URL}/reset-password/${token}</p>`,
         };
 
         return user.updateOne({ resetPasswordLink: token }, (err, success) => {
@@ -91,7 +95,7 @@ exports.forgotPassword = (req, res) => {
             } else {
                 sendEmail(emailData);
                 return res.status(200).json({
-                    message: `Email has been sent to ${email}. Follow the instructions to reset your password.`
+                    message: `Email has been sent to ${email}. Follow the instructions to reset your password.`,
                 });
             }
         });
@@ -110,13 +114,13 @@ exports.resetPassword = (req, res) => {
     User.findOne({ resetPasswordLink }, (err, user) => {
         // if err or no user
         if (err || !user)
-            return res.status('401').json({
-                error: 'Invalid Link!'
+            return res.status("401").json({
+                error: "Invalid Link!",
             });
 
         const updatedFields = {
             password: newPassword,
-            resetPasswordLink: ''
+            resetPasswordLink: "",
         };
 
         user = _.extend(user, updatedFields);
@@ -125,23 +129,32 @@ exports.resetPassword = (req, res) => {
         user.save((err, result) => {
             if (err) {
                 return res.status(400).json({
-                    error: err
+                    error: err,
                 });
             }
             res.json({
-                message: `Great! Now you can login with your new password.`
+                message: `Great! Now you can login with your new password.`,
             });
         });
     });
 };
 
-const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+const client = new OAuth2Client(REACT_APP_GOOGLE_CLIENT_ID);
 
-exports.socialLogin = async (req, res) => {
+exports.socialLogin = async(req, res) => {
     const idToken = req.body.tokenId;
-    const ticket = await client.verifyIdToken({ idToken, audience: process.env.REACT_APP_GOOGLE_CLIENT_ID });
+    const ticket = await client.verifyIdToken({
+        idToken,
+        audience: REACT_APP_GOOGLE_CLIENT_ID,
+    });
     // console.log('ticket', ticket);
-    const { email_verified, email, name, picture, sub: googleid } = ticket.getPayload();
+    const {
+        email_verified,
+        email,
+        name,
+        picture,
+        sub: googleid,
+    } = ticket.getPayload();
 
     if (email_verified) {
         console.log(`email_verified > ${email_verified}`);
@@ -155,8 +168,10 @@ exports.socialLogin = async (req, res) => {
                 req.profile = user;
                 user.save();
                 // generate a token with user id and secret
-                const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME }, process.env.JWT_SECRET);
-                res.cookie('t', token, { expire: new Date() + 9999 });
+                const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME },
+                    process.env.JWT_SECRET
+                );
+                res.cookie("t", token, { expire: new Date() + 9999 });
                 // return response with user and token to frontend client
                 const { _id, name, email } = user;
                 return res.json({ token, user: { _id, name, email } });
@@ -167,8 +182,10 @@ exports.socialLogin = async (req, res) => {
                 user.updated = Date.now();
                 user.save();
                 // generate a token with user id and secret
-                const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME }, process.env.JWT_SECRET);
-                res.cookie('t', token, { expire: new Date() + 9999 });
+                const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME },
+                    process.env.JWT_SECRET
+                );
+                res.cookie("t", token, { expire: new Date() + 9999 });
                 // return response with user and token to frontend client
                 const { _id, name, email } = user;
                 return res.json({ token, user: { _id, name, email } });
